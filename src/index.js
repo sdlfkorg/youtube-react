0effe811a69c27fd7657e525330c9fb106bac3e1
sdlfkorg <sdlfkorg@gmail.com>
2018-04-15 20:41:03 +0800

    index.js: refactor App to class-based component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 // create a new component, that will produce some HTML
 
 const API_KEY = 'AIzaSyCVKYavG0PAlS0B5S-d912Qe0XiKrM3aXg';
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { videos: []};
+		YTSearch({key: API_KEY, term: 'premitive technology'}, (videos) => {
+			this.setState({videos});
+			// equal to this.setState({videos: videos}) in es6
+		});
+	}
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
 		);
+	}
 }
 
 

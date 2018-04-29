203035a04e86c882df4293457c39ca93a78beff0
sdlfkorg <sdlfkorg@gmail.com>
2018-04-29 16:32:58 +0800

    SearchBar: throttling the api search time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search';
@@ -30,9 +31,10 @@ class App extends Component {
 	}
 
 	render(){
+		const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
@@ -46,3 +48,4 @@ class App extends Component {
 // take the component's generated HTML and put it 
 // on the page (DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
+
